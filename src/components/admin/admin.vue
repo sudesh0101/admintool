5d15a7c216cc9ce53fddfa9af114cacec3d5d401
Abhishek <abhiatstudy@gmail.com>
2017-08-30 00:56:13 +0530

    project setup admintool

---
+++ src/components/admin/admin.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+   Admin Page
+  </div>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
---
+++ src/routes.js
@@ -0,0 +1,6 @@
+import Admin from './components/admin/admin.vue'
+
+export const routes = [
+    { path:'',component:Admin},
+    { path:'/admin',component:Admin}
+]
